7af531ea3200e29c5730a13422ec23bab86c409f
nafeelxhussain-sys <nafeelxhussain@gmail.com>
2026-09-15 01:45:08 +0530

    Add: Helper functions and const keyword for variables

--- include/vector.hpp
+++ include/vector.hpp
@@ -11,7 +11,14 @@ class collection{
 
     public:
     collection(int dim);
-    bool insert_vector(std::string &id, std::vector<float> &vector);
-    bool remove_vector(std::string &id);
-    std::vector<float> get_vector(std::string &id);
+    bool insert_vector(const std::string &id, const std::vector<float> &vector);
+    bool remove_vector(const std::string &id);
+    std::vector<float> get_vector(const std::string &id) const;
+    std::vector<float> get_vector(const uint64_t index) const;
+
+    //Helper functions to access private members
+    inline bool _isdead (const uint64_t index) const { return isdead[index]; };   // return true if data[index] is dead
+    inline uint32_t _dim() const { return dim; };                                 // return dimention
+    inline uint32_t _size() const { return data.size()/_dim(); };                 // returns number of vectors in collection
+    inline uint64_t _offset(const std::string &id) const;                         // return offset to an index
 };
--- src/vector.cpp
+++ src/vector.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 collection :: collection(int dim): dim(dim){}
 
-bool collection::insert_vector(string &id,vector<float> &inputVector){
+bool collection::insert_vector(const string &id, const vector<float> &inputVector){
     if(maps.count(id))
         return false; //todo
     if(inputVector.size()!=dim)
@@ -16,23 +16,25 @@ bool collection::insert_vector(string &id,vector<float> &inputVector){
     return true;
 }
 
-bool collection :: remove_vector(string &id){
-    if(!maps.count(id))
-        return false; //execption case
+bool collection :: remove_vector(const string &id){
+    auto iter = maps.find(id);
+    if(iter == maps.end())
+        return false; //todo
 
-    uint32_t offset = maps[id];
+    uint64_t offset = iter->second;
     uint32_t index = offset/dim;
     isdead[index]=true;
     return true;
 }
 
-vector<float> collection :: get_vector(string &id){
+vector<float> collection :: get_vector(const string &id) const{
     vector<float> returnVector(dim, 0.0);
 
-    if(!maps.count(id))
+    auto iter = maps.find(id);
+    if(iter == maps.end())
         return returnVector; //todo
 
-    uint64_t offset = maps[id];
+    uint64_t offset = iter->second;
     uint32_t index = offset/dim;
 
     if(isdead[index])
@@ -42,4 +44,28 @@ vector<float> collection :: get_vector(string &id){
         returnVector[i] = data[offset + i];
     }
     return returnVector;
+}
+
+std::vector<float> collection :: get_vector(const uint64_t index) const{
+    vector<float> returnVector(_dim(), 0.0);
+    uint64_t offset = index * _dim();
+
+    if(isdead[index])
+        return returnVector; //todo
+
+    for(size_t i = 0 ; i < _dim() ; i++){
+        returnVector[i] = data[offset+i];
+    }
+    
+    return returnVector;
+}
+
+inline uint64_t collection :: _offset(const std::string &id) const{
+    auto iter = maps.find(id);
+    if (iter == maps.end()) {
+        // Return max 64bit int //todo
+        return std::numeric_limits<uint64_t>::max();
+    }
+    
+    return iter->second;
 }
